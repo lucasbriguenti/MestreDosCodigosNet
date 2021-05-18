72bf61adbcc01f7a1b74cf5be2e081fcfe3612b9
Lucas Briguenti <lucas.briguenti@db1.com.br>
2021-05-18 13:59:36 -0300

    Adicionado Teste de idade da pessoa

---
+++ MestreDosCodigosLucas/TesteNUnit/Tests/PessoaTests.cs
@@ -0,0 +1,23 @@
+﻿using MestreDosCodigosPOOLucas.Modulos.PessoaModulo;
+using NUnit.Framework;
+using System;
+
+namespace TesteNUnit.Tests
+{
+    [TestFixture]
+    public class PessoaTests
+    {
+        private Pessoa _pessoa;
+
+        [Test]
+        public void DeveCalcularIdadeCorretamente()
+        {
+            var anoNascimento = 40;
+            var dataDeNascimento = DateTime.Now.AddYears(-anoNascimento);
+            var idadeEsperada = DateTime.Now.DayOfYear < dataDeNascimento.DayOfYear ? anoNascimento - 1 : anoNascimento;
+            _pessoa = new Pessoa(null, dataDeNascimento, 1.7);
+
+            Assert.AreEqual(idadeEsperada, _pessoa.Idade);
+        }
+    }
+}
